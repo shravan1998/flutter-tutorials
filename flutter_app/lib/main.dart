2902a54d7c00035de873406f8805667e0c736d8d
shravan1998 <shrkum51@gmail.com>
2019-10-03 09:26:55 +0530

    Scaffold added

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Text('hello World'),
+  home:Scaffold(
+    appBar:AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('hello world'),
+
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+    ),
+  ),
 ));
 
