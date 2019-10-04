a36263886a94b1902507ab27b01d775fd0d76baf
shravan1998 <shrkum51@gmail.com>
2019-10-04 13:00:28 +0530

    Added image

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Scaffold(
-    appBar:AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.deepPurple,
-    ),
-    body: Center(
-      child: Text('hello world'),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-    ),
-  ),
+  home:Home()
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://images.unsplash.com/photo-1568369160472-27f9997995e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80'),
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('click'),
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+}
 
